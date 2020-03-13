851a08c9af2e17e233d4eae7b5c4c9ec8469c1fe
kaiquecirto <kaique.cirto.mafra@gmail.com>
2020-03-13 18:55:21 -0300

    Adicionando inserção de questões
    Adicionando inserção de questões

--- controllers/api/questions.controller.js
+++ controllers/api/questions.controller.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var questionService = require('services/question.service');
 
 router.get('/getAll', getAll);
+router.post('/register', createQuestion);
 
 module.exports = router;
 
@@ -11,6 +12,16 @@ function getAll(req, res) {
     questionService.getAll()
         .then(function (questions) {
             res.send(questions);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function createQuestion(req, res) {
+    questionService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
         })
         .catch(function (err) {
             res.status(400).send(err);
